70f759f11265d456ca1a9b04eff3850c6affee42
Seth McClaine <smcclaine@smcclaine-mbr.f4tech.com>
2016-01-29 08:50:16 -0700

    add example of config 	new file:   exampleConfig.js

---
+++ exampleConfig.js
@@ -0,0 +1,10 @@
+//Duplicate this file and save as config to start project
+module.exports = {
+        cred: '',//Flowdock users API Key
+        pass: 'password',//Password is not evaluated so it can be anything
+        org: 'rally-software', //Orginazation, most likely will always be Rally
+        flow: 'amazingwalnuts' //`parameterized_name` of flow... 
+
+//List of flows can be found with
+//https://*apikey*:password@api.flowdock.com/flows
+};
